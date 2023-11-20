dabaa841676f8131920c693afe27c2151a9167a4
Sokwva <1519181530@qq.com>
2023-11-20 21:30:39 +0800

    checkHealthy

--- basic.go
+++ basic.go
@@ -9,13 +9,17 @@ import (
 	"sokwva/KC030V3-shifu-driver/server"
 	"sokwva/KC030V3-shifu-driver/utils"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/urfave/cli/v2"
 )
 
 var (
-	target string = ""
+	target      string = ""
+	healthCheck string = ""
+	//环境：container or host 容器环境下的健康检测若遇到错误会直接panic出错，主机环境下只是打印错误信息
+	enviroment string = "container"
 )
 
 func commonTail(queryCmd *serializer.PacketStruct, c *gin.Context) {
@@ -144,7 +148,30 @@ func probeDevice(c *gin.Context) {
 	}
 }
 
+func checkHealthy() {
+	timer := time.NewTicker(time.Second * 10)
+	for {
+		<-timer.C
+		if utils.ProbeTCP(target) {
+			continue
+		} else {
+			if enviroment == "container" {
+				//容器环境下可以直接panic
+				panic("device is not healthy")
+			}
+			if enviroment == "host" {
+				//主机环境下可以直接打印错误信息
+				fmt.Println("device is not healthy")
+			}
+		}
+	}
+}
+
 func serve() {
+	if healthCheck == "true" {
+		go checkHealthy()
+	}
+
 	ginRoot := gin.Default()
 
 	ginRoot.GET("/ping", probeDevice)
@@ -167,6 +194,18 @@ func main() {
 				Value:       "192.168.1.20:8080",
 				Usage:       "target and port of target device.",
 				Destination: &target,
+			},
+			&cli.StringFlag{
+				Name:        "check",
+				Value:       "false",
+				Usage:       "loop to check target device healthy.",
+				Destination: &healthCheck,
+			},
+			&cli.StringFlag{
+				Name:        "env",
+				Value:       "container",
+				Usage:       "different enviroment of different unhealthy action.",
+				Destination: &enviroment,
 			},
 		},
 		Action: func(ctx *cli.Context) error {
