caeb03bac5a7686e9c4bea35e99aaee28ad8f981
Sokwva <1519181530@qq.com>
2024-06-18 12:42:45 +0800

    reverse cmd

--- client/base.go
+++ client/base.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"net"
+	"slices"
 	"time"
 )
 
@@ -9,15 +10,16 @@ func Send(target string, cmd []byte) ([]byte, error) {
 	tcpConn, err := net.DialTimeout("tcp", target, 5*time.Second)
 
 	if err != nil {
-		panic(err)
+		return []byte{}, err
 	}
 	defer tcpConn.Close()
 
+	slices.Reverse(cmd)
 	tcpConn.Write(cmd)
 	var buf []byte = make([]byte, 20)
 	_, err = tcpConn.Read(buf)
 	if err != nil {
-		panic(err)
+		return []byte{}, err
 	}
 	return buf, err
 }
